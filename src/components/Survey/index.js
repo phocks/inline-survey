49761841255cf4599bd74821b293247a6f69ff6a
Joshua Byrd <phocks@gmail.com>
2020-01-17 16:07:02 +1000

    Formatting

--- src/components/Survey/index.js
+++ src/components/Survey/index.js
@@ -3,10 +3,9 @@ import styles from "./styles.scss";
 import Question from "../Question";
 
 export default props => {
-
   const handleChange = data => {
-    console.log(data)
-  }
+    console.log(data);
+  };
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -16,8 +15,16 @@ export default props => {
   return (
     <div className={styles.root}>
       <form onSubmit={handleSubmit}>
-        <Question text="Had more money" questionId={"Q69_1"} onChange={handleChange} />
-        <Question text="Were more involved in my community" questionId={"Q69_10"} onChange={handleChange} />
+        <Question
+          text="Had more money"
+          questionId={"Q69_1"}
+          onChange={handleChange}
+        />
+        <Question
+          text="Were more involved in my community"
+          questionId={"Q69_10"}
+          onChange={handleChange}
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
