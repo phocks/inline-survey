5535d77368aa269c12655e19eda9c98d05c9ba2a
Joshua Byrd <phocks@gmail.com>
2020-01-21 10:30:52 +1000

    Value

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -5,7 +5,7 @@ export default props => {
   const { text, questionId = "default", onChange } = props;
 
   const handleChange = event => {
-    onChange(event.target.id);
+    onChange({ questionId: questionId, selection: event.target.value });
   };
 
   return (
@@ -14,6 +14,7 @@ export default props => {
       <input
         type="radio"
         id={`${questionId}-strongly-disagree`}
+        value={"strongly-disagree"}
         name={questionId}
       />
       <label htmlFor={`${questionId}-strongly-disagree`}>
@@ -23,25 +24,38 @@ export default props => {
         type="radio"
         id={`${questionId}-somewhat-disagree`}
         name={questionId}
+        value={"somewhat-disagree"}
       />
       <label htmlFor={`${questionId}-somewhat-disagree`}>
         Somewhat disagree
       </label>
-      <input type="radio" id={`${questionId}-neutral`} name={questionId} />
+      <input
+        type="radio"
+        id={`${questionId}-neutral`}
+        name={questionId}
+        value={"neutral"}
+      />
       <label htmlFor={`${questionId}-neutral`}>Neutral</label>
       <input
         type="radio"
         id={`${questionId}-somewhat-agree`}
         name={questionId}
+        value={"somewhat-agree"}
       />
       <label htmlFor={`${questionId}-somewhat-agree`}>Somewhat agree</label>
       <input
         type="radio"
         id={`${questionId}-strongly-agree`}
         name={questionId}
+        value={"strongly-agree"}
       />
       <label htmlFor={`${questionId}-strongly-agree`}>Strongly agree</label>
-      <input type="radio" id={`${questionId}-dont-know`} name={questionId} />
+      <input
+        type="radio"
+        id={`${questionId}-dont-know`}
+        name={questionId}
+        value={"dont-know"}
+      />
       <label htmlFor={`${questionId}-dont-know`}>Don't know</label>
     </div>
   );
