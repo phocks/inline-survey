1827b1e2b07a1582869fe677acd0d7ec856d5b99
Joshua Byrd <phocks@gmail.com>
2020-02-21 13:37:28 +1000

    Temporary arrow

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,33 @@
 import React, { useEffect, useRef } from "react";
 import styles from "./styles.scss";
 import Survey from "../Survey";
+import { Portal } from "react-portal";
 import { addClass } from "../../lib/utils";
 
-import AnimatedIcons from "../AnimatedIcons"
+import AnimatedIcons from "../AnimatedIcons";
+
+const UpArrow = () => (
+  <div className="test">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <path d="M7 11v13h2v-5h2v3h2v-7h2v9h2v-13h6l-11-11-11 11z" />
+    </svg>
+  </div>
+);
 
 export default props => {
   return (
     <div className={styles.root}>
+      <Portal node={document.querySelector(".interactivefooter")}>
+        <center>
+          <UpArrow />
+        </center>
+      </Portal>
+
       <AnimatedIcons />
       <Survey />
     </div>
