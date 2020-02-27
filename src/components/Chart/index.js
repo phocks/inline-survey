117ba5fed8b5161b57920ef0310bd4d7e60ab1c5
Joshua Byrd <phocks@gmail.com>
2020-02-27 15:37:48 +1000

    Flexbox opinions

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import styles from './styles.scss';
+import React from "react";
+import styles from "./styles.scss";
 
 export default props => {
   return (
     <div className={styles.root}>
       <div className={styles.container}>
-
-      <h3>{props.heading}</h3>
-      <div className={styles.opinions}>
-        <div className={styles.opinion}>STRONGLY AGREE</div>
-        <div className={styles.opinion}>SOMEWHAT DISAGREE</div>
-        <div className={styles.opinion}>NEUTRAL</div>
-        <div className={styles.opinion}>SOMEWHAT AGREE</div>
-        <div className={styles.opinion}>STRONGLY AGREE</div>
+        <h3>{props.heading}</h3>
+        <div className={styles.opinions}>
+          <div className={styles.opinion}>STRONGLY AGREE</div>
+          <div className={styles.opinion}>SOMEWHAT DISAGREE</div>
+          <div className={styles.opinion}>NEUTRAL</div>
+          <div className={styles.opinion}>SOMEWHAT AGREE</div>
+          <div className={styles.opinion}>STRONGLY AGREE</div>
+        </div>
       </div>
-
-      </div>
-      
     </div>
   );
-}
+};
