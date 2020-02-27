6501dbedbac9992099d928d8172a5b6eefa2ca2b
Joshua Byrd <phocks@gmail.com>
2020-02-27 16:55:01 +1000

    Dot containers

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import styles from "./styles.scss";
 
 export default props => {
+  const filteredData = props.data.filter(entry => {
+    return entry.question === props.questionId && entry.group === props.group;
+  });
+
+  console.log(filteredData);
+
   return (
     <div className={styles.root}>
       <div className={styles.container}>
@@ -12,6 +18,23 @@ export default props => {
           <div className={styles.opinion}>NEUTRAL</div>
           <div className={styles.opinion}>SOMEWHAT AGREE</div>
           <div className={styles.opinion}>STRONGLY AGREE</div>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.dotContainer}>
+            <div className={styles.dot}></div>
+          </div>
+          <div className={styles.dotContainer}>
+            <div className={styles.dot}></div>
+          </div>
+          <div className={styles.dotContainer}>
+            <div className={styles.dot}></div>
+          </div>
+          <div className={styles.dotContainer}>
+            <div className={styles.dot}></div>
+          </div>
+          <div className={styles.dotContainer}>
+            <div className={styles.dot}></div>
+          </div>
         </div>
       </div>
     </div>
