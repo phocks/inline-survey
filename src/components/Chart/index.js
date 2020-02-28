c58e8b8662bbe1915fe113ea6b3cecb626ee42a6
Joshua Byrd <phocks@gmail.com>
2020-02-28 12:16:43 +1000

    vertical center

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { scaleLinear } from "d3-scale";
+
 import styles from "./styles.scss";
+
+const scale = scaleLinear()
+  .domain([0.0, 1.0])
+  .range([1, 50]);
 
 export default props => {
   const filteredData = props.data.filter(entry => {
@@ -7,6 +13,16 @@ export default props => {
   });
 
   console.log(filteredData);
+
+  let chartData = {};
+
+  for (const item of filteredData) {
+    if (typeof chartData[item.by_group] === "undefined")
+      chartData[item.by_group] = [];
+    chartData[item.by_group].push(item.value);
+  }
+
+  console.log(chartData);
 
   return (
     <div className={styles.root}>
@@ -19,28 +35,60 @@ export default props => {
           <div className={styles.opinion}>SOMEWHAT AGREE</div>
           <div className={styles.opinion}>STRONGLY AGREE</div>
         </div>
-        <div className={styles.row}>
-        <div className={styles.line}></div>
-          <div className={styles.dotContainer}>
-            <div
-              className={styles.dot}
-              style={{ width: "30px", height: "30px" }}
-            ></div>
-          </div>
-          <div className={styles.dotContainer}>
-            <div className={styles.dot}></div>
-          </div>
-          <div className={styles.dotContainer}>
-            <div className={styles.dot}></div>
-          </div>
-          <div className={styles.dotContainer}>
-            <div className={styles.dot}></div>
-          </div>
-          <div className={styles.dotContainer}>
-            <div className={styles.dot}></div>
-          </div>
-          
-        </div>
+
+        {Object.keys(chartData).map((row, iteration) => {
+          console.log(chartData[row]);
+          return (
+            <div className={styles.row}>
+              <div className={styles.line}></div>
+              <div className={styles.dotContainer}>
+                <div
+                  className={styles.dot}
+                  style={{
+                    width: Math.round(scale(chartData[row][0])) + "px",
+                    height: Math.round(scale(chartData[row][0])) + "px"
+                  }}
+                ></div>
+              </div>
+              <div className={styles.dotContainer}>
+                <div
+                  className={styles.dot}
+                  style={{
+                    width: Math.round(scale(chartData[row][1])) + "px",
+                    height: Math.round(scale(chartData[row][1])) + "px"
+                  }}
+                ></div>
+              </div>
+              <div className={styles.dotContainer}>
+                <div
+                  className={styles.dot}
+                  style={{
+                    width: Math.round(scale(chartData[row][2])) + "px",
+                    height: Math.round(scale(chartData[row][2])) + "px"
+                  }}
+                ></div>
+              </div>
+              <div className={styles.dotContainer}>
+                <div
+                  className={styles.dot}
+                  style={{
+                    width: Math.round(scale(chartData[row][3])) + "px",
+                    height: Math.round(scale(chartData[row][3])) + "px"
+                  }}
+                ></div>
+              </div>
+              <div className={styles.dotContainer}>
+                <div
+                  className={styles.dot}
+                  style={{
+                    width: Math.round(scale(chartData[row][4])) + "px",
+                    height: Math.round(scale(chartData[row][4])) + "px"
+                  }}
+                ></div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
